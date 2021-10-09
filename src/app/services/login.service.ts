1704880fda477031863d66895f96c6225d4192ca
Bhola Kumar <bholakumar.7372@gmail.com>
2021-10-09 17:05:32 +0530

    Login Application using Angular

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { ThrowStmt } from '@angular/compiler';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  url = "http://localhost:8080"
+
+  constructor(private http: HttpClient) { }
+
+  //calling the server to generate token
+  generateToken(credentials: any) {
+    //token generate
+    return this.http.post(`${this.url}/token`, credentials);
+  }
+
+
+
+  //for login user
+  loginUser(token:any) {
+    localStorage.setItem("token", token);
+    return true;
+  }
+
+  //to check user is logged in or not
+  isLoggedIn() {
+    let token = localStorage.getItem("token");
+    if (token == undefined || token == '' || token == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  //for logout the user
+  logout() {
+    localStorage.removeItem("token");
+    return true;
+  }
+
+  //for getting the token
+  getToken() {
+    return localStorage.getItem("token");
+  }
+}
